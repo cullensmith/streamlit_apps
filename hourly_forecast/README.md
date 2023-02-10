6cc3f4482704b0c20efd9849af1269ba5d656c48
Cullen <smith.cmaps@gmail.com>
2023-02-10 18:24:28 -0500

    altered styles

---
+++ hourly_forecast/README.md
@@ -0,0 +1,9 @@
+# Generic web application
+## Purpose
+    Show basic funcitonality of a web app that utilizes an API request to populate a chart. Takes it a step further allowing the user to make a selection based upon input of a slider. Aesthetics are currently an afterthought and can be updated at a later date.
+
+## Usage
+- Click anywhere on the map
+- Sends a request to weather.gov using their API
+- Parses the response and populates a line chart depicting the hourly forecasted temperature
+- Drag the slider to select a specfic hour within the forecast
--- hourly_forecast/src/streamlit_ex_weather.py
+++ hourly_forecast/src/streamlit_ex_weather.py
@@ -2,15 +2,17 @@ import streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
+import matplotlib as plt
+import os
 
 from weather import get_cell as cell, get_forecast as get_f
 
+print(plt.get_configdir())
 
 def display_map():
     map = folium.Map(location=[40.823740, -77.862548], zoom_start=12, scrollWheelZoom=False, tiles='CartoDB positron')
     st_map = st_folium(map, width=700, height=450)
     return st_map
-
 
 def get_pos(lat,lng):
     return lat,lng
@@ -24,16 +26,28 @@ def get_f15(y,x):
 
 def main():
     st.write('Hourly Forecast')
+    
     map = display_map()
-
+    # plt.pyplot.rcParams.update(plt.rcParamsDefault)
+    plt.pyplot.style.use('chart_styler.mplstyle')
     if map['last_clicked'] is not None:
         coordinates = get_pos(map['last_clicked']['lat'],map['last_clicked']['lng'])
         try:
+            plt.pyplot.grid(True)
             forecast = get_f15(coordinates[0],coordinates[1])
-            st.line_chart(forecast['temperature'], y = 'temperature')
+            # st.line_chart(forecast['temperature'], y = 'temperature')
+            fig, ax = plt.pyplot.subplots()
             temp = st.select_slider(label='Check Specific Hour:',options=forecast['startTime'], format_func=lambda x: x.strftime('%A %b %d: %I%p') )
-            st.write(f"forecasted temp: {forecast[forecast.startTime == temp]['temperature'].iloc[0]}")
-        except KeyError:
+            st.write(f"Forecasted Temp: {forecast[forecast.startTime == temp]['temperature'].iloc[0]}")
+            ax.plot(forecast['temperature'],linewidth=1.5)
+            read_temp = forecast[forecast.startTime == temp]['number'].iloc[0]
+            ax.plot(forecast['temperature'][:read_temp], linewidth=2, color='#ff7b7b', zorder=2)
+            ax.scatter(forecast[forecast.startTime == temp]['startTime'].iloc[0], forecast[forecast.startTime == temp]['temperature'].iloc[0],color='#ff6b6b', zorder=3)
+            rfdates = plt.dates.DateFormatter('%a')
+            ax.xaxis.set_major_formatter(rfdates)
+            st.pyplot(fig)
+        except KeyError as e:
+            # print(e)
             st.write('There was an error, please click the map again. Thanks')
 if __name__ == '__main__':
     main()
