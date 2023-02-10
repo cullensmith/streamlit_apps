7e7297ab9510b0af868e490613ac95c65e8c8a05
Cullen <smith.cmaps@gmail.com>
2023-02-10 18:59:45 -0500

    updated style

---
+++ hourly_forecast/src/chart_style.py
@@ -0,0 +1,22 @@
+import matplotlib as plt
+
+def set_style(fig):
+    plt.pyplot.grid(True)
+    plt.pyplot.rcParams.update({'grid.color': '#eeeeee'})
+    plt.pyplot.rcParams.update({'font.family': 'sans=serif'})
+    plt.pyplot.rcParams.update({'font.size': 8})
+    plt.pyplot.rcParams.update({'axes.labelcolor': 'red'})
+    plt.pyplot.rcParams.update({'axes.edgecolor': '#b4b4b4'})
+    plt.pyplot.rcParams.update({'axes.titlecolor': 'yellow'})
+    plt.pyplot.rcParams.update({'axes.facecolor': 'white'})
+    plt.pyplot.rcParams.update({'axes.spines.left':   True})
+    plt.pyplot.rcParams.update({'axes.spines.bottom':   True})
+    plt.pyplot.rcParams.update({'axes.spines.top':   False})
+    plt.pyplot.rcParams.update({'axes.spines.right':   False})
+    plt.pyplot.rcParams.update({'xtick.labelcolor': '#9b9b9b'})
+    plt.pyplot.rcParams.update({'ytick.labelcolor': '#7b7b7b'})
+    plt.pyplot.rcParams.update({'xtick.color': '#9b9b9b'})
+    plt.pyplot.rcParams.update({'ytick.color': '#9b9b9b'})
+    plt.pyplot.rcParams.update({'ytick.color': '#9b9b9b'})
+
+# set_style()
--- hourly_forecast/src/streamlit_ex_weather.py
+++ hourly_forecast/src/streamlit_ex_weather.py
@@ -3,11 +3,14 @@ import pandas as pd
 import folium
 from streamlit_folium import st_folium
 import matplotlib as plt
-import os
+import chart_style
 
 from weather import get_cell as cell, get_forecast as get_f
 
-print(plt.get_configdir())
+# def style_chart():
+#     chart_style.set_style()
+#     print('style is set')
+    
 
 def display_map():
     map = folium.Map(location=[40.823740, -77.862548], zoom_start=12, scrollWheelZoom=False, tiles='CartoDB positron')
@@ -28,17 +31,20 @@ def main():
     st.write('Hourly Forecast')
     
     map = display_map()
-    # plt.pyplot.rcParams.update(plt.rcParamsDefault)
-    plt.pyplot.style.use('chart_styler.mplstyle')
+    
+
     if map['last_clicked'] is not None:
         coordinates = get_pos(map['last_clicked']['lat'],map['last_clicked']['lng'])
         try:
             plt.pyplot.grid(True)
             forecast = get_f15(coordinates[0],coordinates[1])
-            # st.line_chart(forecast['temperature'], y = 'temperature')
+
             fig, ax = plt.pyplot.subplots()
+            chart_style.set_style(fig)
+
             temp = st.select_slider(label='Check Specific Hour:',options=forecast['startTime'], format_func=lambda x: x.strftime('%A %b %d: %I%p') )
             st.write(f"Forecasted Temp: {forecast[forecast.startTime == temp]['temperature'].iloc[0]}")
+
             ax.plot(forecast['temperature'],linewidth=1.5)
             read_temp = forecast[forecast.startTime == temp]['number'].iloc[0]
             ax.plot(forecast['temperature'][:read_temp], linewidth=2, color='#ff7b7b', zorder=2)
@@ -47,7 +53,6 @@ def main():
             ax.xaxis.set_major_formatter(rfdates)
             st.pyplot(fig)
         except KeyError as e:
-            # print(e)
             st.write('There was an error, please click the map again. Thanks')
 if __name__ == '__main__':
     main()
